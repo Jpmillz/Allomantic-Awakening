6c13c25bff26032963a0907ce5dad31a87596c9b
jpmillz <legitmillz@gmail.com>
2024-11-28 14:26:55 -0600

    Commit

---
+++ src/main/java/io/github/jpmillz/allomanticawakening/AllomanticAwakening.java
@@ -0,0 +1,39 @@
+package io.github.jpmillz.allomanticawakening;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(AllomanticAwakening.MODID)
+public class AllomanticAwakening {
+
+    public static final String MODID = "allomanticawakening";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AllomanticAwakening(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
